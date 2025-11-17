4720153e473c73c48c55eaa573bca6773ce304a4
sherix <mrhyder29@gmail.com>
2025-11-17 16:00:42 +0500

    added some code for learning

---
+++ index.php
@@ -0,0 +1,120 @@
+ <?php
+// $name = "haider";
+// echo $name;
+// echo "<br>";
+// echo "My name is $name."; 
+// echo "<br>";
+// $a = 34;
+// $b = 45;
+// $sum = $a+$b;
+// echo $sum;
+// echo "<br>";
+// echo "the data type of sum is :";var_dump($sum);
+
+// $num = 49;
+// $num2 = 34;
+// if($num < 50){
+//     echo "number is less than 50.";
+// }
+// elseif($num >=50 && $num <80){
+//     echo "number is greater than or equal to 50.";
+//     echo '<br>';
+// }
+// else{
+//     echo "number is 80 or greater than 80.";
+//      echo '<br>';
+// }
+
+// switch($num){
+//     case $num<50:
+//         echo "Number is less than 50";
+//         break;
+//     default:
+//         echo "nothing";
+// }
+
+// $marks = [34,70,56,78,"ali"];
+// echo "<hr>";
+// print_r($marks);
+// array_pop($marks);
+// echo "<hr>";
+// print_r($marks);
+// array_push($marks,"pushed");
+// echo "<hr>";
+// print_r($marks);
+// echo "<hr>";
+// $sub = ["phy"=>50,"eng"=>54,"math"=>76];
+// print_r($sub);
+// echo "<hr>";
+// print_r($sub["math"])
+
+// $arr = [
+//     ['name' => 'ali', 'age' => 20],
+//     ['name' => 'hasan', 'age' => 22],
+//     ['name' => 'zain', 'age' => 18]
+// ];
+// print ($arr[0]['age']);
+
+// $code = 100;
+// $result = match($code){
+//     100=>'ok',
+//     400=>'error',
+//     default=> 'nothing'
+// };
+// echo $result;
+
+//  $i = 0;
+// while($i<=10){
+//     echo $i;
+//     $i++;
+//     echo '<hr>';
+// }
+
+// do{
+//     echo $i;
+//     echo "<br>";
+//     $i++;
+// }while($i<=100);
+
+// for($i = 0; $i<=10; $i++){
+//     echo $i;
+//     echo '<hr>';
+// }
+
+// $arr = [1,2,3,4,5,6,7,8,9,10];
+// echo "the values in the array are: <br>";
+// foreach($arr as $value){
+//     echo $value;
+//     echo '<br>';
+// }
+
+// $name = ['ali','haider','fatima','hasan','zain'];
+// foreach($name as $names){
+//     echo "Now the name is: $names. <hr>";
+// }
+
+// function greeting($name){
+//     return "Hello, $name";
+// }
+// echo greeting("haider");
+
+// function sum($num1 = 23,$num2 = 23){
+//     return $num1+$num2;
+// }
+// echo "The sum of 5 and 7 is ". sum(5,7).'.';
+
+// define('PI',3.14);
+// echo PI;
+
+// $greet = function(){
+//     return "hello bro wsup!!!😉";
+// };
+// echo $greet();
+
+// $arr = [1,2,3,4,5];
+// echo '<pre>';
+// print_r($arr);
+// echo '</pre>';
+
+
+?> 

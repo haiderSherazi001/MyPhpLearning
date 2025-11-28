ae80999d3812d404ec52a77e69476592928879a3
sherix <mrhyder29@gmail.com>
2025-11-28 13:27:38 +0500

    some more code of get and post

--- phpGlobals/verify.php
+++ phpGlobals/verify.php
@@ -1,17 +1,11 @@
 <?php
 $username = "mrhyder_29";
 $password = 8989;
-$count = 3;
-while($count>0){
 if($_POST['username'] == $username && $_POST['password'] == $password){
     echo "You have entered correct information! <br>";
-    break;
 }
-
 else{
     echo "Incorrect password or username! <br>";
     echo "<a href = form.html>try again</a><br>";
-    $count--;
-}
 }
 ?>
